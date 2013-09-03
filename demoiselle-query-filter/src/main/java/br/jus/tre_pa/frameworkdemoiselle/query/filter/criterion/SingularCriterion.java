c929a798102e70c15b5710f83e690c8103924973
jcruz <jcruz@jcruz.jcruz>
2013-09-03 01:00:26 -0300

    pacote criterion

---
+++ demoiselle-query-filter/src/main/java/br/jus/tre_pa/frameworkdemoiselle/query/filter/criterion/SingularCriterion.java
@@ -0,0 +1,30 @@
+package br.jus.tre_pa.frameworkdemoiselle.query.filter.criterion;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import br.jus.tre_pa.frameworkdemoiselle.query.filter.internal.AbstractSingularQueryCriterion;
+import br.jus.tre_pa.frameworkdemoiselle.query.filter.operation.SingularOperation;
+
+public class SingularCriterion<X> extends AbstractSingularQueryCriterion<X> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public SingularCriterion(String field, SingularOperation<X> operation) {
+		super(field, operation);
+	}
+
+	@Override
+	public <T> Predicate asPredicate(CriteriaBuilder cb, Root<T> p) {
+		return !getField().isEmpty() && getValue() != null ? executeOperation(cb, p) : null;
+	}
+
+	private <T> Predicate executeOperation(CriteriaBuilder cb, Root<T> p) {
+		return getOperation().execute(cb, p, getField(), getValue());
+	}
+
+}
