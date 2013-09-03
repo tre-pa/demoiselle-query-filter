04f7a80ff4a8a204379bea1b2ffd19ba07c2882e
jcruz <jcruz@jcruz.jcruz>
2013-09-03 00:53:49 -0300

    Criacao de classe Helper

---
+++ demoiselle-query-filter/src/main/java/br/jus/tre_pa/frameworkdemoiselle/query/filter/helper/JPAQueryHelper.java
@@ -0,0 +1,31 @@
+package br.jus.tre_pa.frameworkdemoiselle.query.filter.helper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.Order;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import br.jus.tre_pa.frameworkdemoiselle.query.filter.context.CriteriaContext;
+import br.jus.tre_pa.frameworkdemoiselle.query.filter.internal.AbstractQueryCriterion;
+
+public class JPAQueryHelper {
+
+	@Inject
+	private CriteriaContext context;
+
+	public <T> Predicate[] getPredicates(CriteriaBuilder cb, Root<T> p, Class<?> filterClass) {
+		List<Predicate> predicates = new ArrayList<Predicate>();
+		for (AbstractQueryCriterion criterion : context.get(filterClass)) {
+			Predicate predicate = criterion.asPredicate(cb, p);
+			if (predicate != null) {
+				predicates.add(predicate);
+			}
+		}
+		return !predicates.isEmpty() ? predicates.toArray(new Predicate[] {}) : null;
+	}
+
+}
