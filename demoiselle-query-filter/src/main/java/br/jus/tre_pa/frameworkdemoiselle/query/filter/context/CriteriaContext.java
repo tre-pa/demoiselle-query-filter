8f31da7625208bd3e57c381bcb2d45bfa3e464f6
jcruz <jcruz@jcruz.jcruz>
2013-09-03 00:58:44 -0300

    alteracao do nome do metodo de add para put

--- demoiselle-query-filter/src/main/java/br/jus/tre_pa/frameworkdemoiselle/query/filter/context/CriteriaContext.java
+++ demoiselle-query-filter/src/main/java/br/jus/tre_pa/frameworkdemoiselle/query/filter/context/CriteriaContext.java
@@ -2,17 +2,11 @@ package br.jus.tre_pa.frameworkdemoiselle.query.filter.context;
 
 import java.io.Serializable;
 import java.util.Collection;
-import java.util.Map;
 
-import br.jus.tre_pa.frameworkdemoiselle.query.filter.core.AbstractQueryCriterion;
+import br.jus.tre_pa.frameworkdemoiselle.query.filter.internal.AbstractQueryCriterion;
 
 public interface CriteriaContext extends Serializable {
-	public void add(Class<?> filterClass, AbstractQueryCriterion criterion);
+	public void put(Class<?> filterClass, AbstractQueryCriterion criterion);
 
 	public Collection<AbstractQueryCriterion> get(Class<?> filterClass);
-	
-	public Map<String, String> getFilters();
-	
-	public void setFilters(Map<String, String> filters);
-	
 }
