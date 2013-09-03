040a236f9e496e93f259444fd2586d9d0b856379
jcruz <jcruz@jcruz.jcruz>
2013-09-03 01:00:40 -0300

    del

--- demoiselle-query-filter/src/main/java/br/jus/tre_pa/frameworkdemoiselle/query/filter/core/JPAQueryFilter.java
+++ demoiselle-query-filter/src/main/java/br/jus/tre_pa/frameworkdemoiselle/query/filter/core/JPAQueryFilter.java
@@ -1,9 +1,7 @@
 package br.jus.tre_pa.frameworkdemoiselle.query.filter.core;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import javax.enterprise.context.ContextNotActiveException;
 import javax.inject.Inject;
@@ -18,7 +16,7 @@ import br.gov.frameworkdemoiselle.pagination.Pagination;
 import br.gov.frameworkdemoiselle.pagination.PaginationContext;
 import br.gov.frameworkdemoiselle.util.Beans;
 import br.gov.frameworkdemoiselle.util.Reflections;
-import br.jus.tre_pa.frameworkdemoiselle.query.filter.context.CriteriaContext;
+import br.jus.tre_pa.frameworkdemoiselle.query.filter.helper.JPAQueryHelper;
 
 public class JPAQueryFilter<T> implements Serializable {
 
@@ -29,30 +27,15 @@ public class JPAQueryFilter<T> implements Serializable {
 
 	private EntityManager entityManager;
 
-	@Inject
-	private CriteriaContext context;
-
 	private Pagination pagination;
 
 	private Class<T> beanClass;
 
+	@Inject
+	private JPAQueryHelper helper;
+
 	public JPAQueryFilter() {
 		super();
-	}
-
-	protected Predicate[] getPredicates(CriteriaBuilder cb, Root<T> p) {
-		List<Predicate> predicates = new ArrayList<Predicate>();
-		for (AbstractQueryCriterion criterion : this.context.get(this.getClass())) {
-			Predicate predicate = criterion.getPredicate(cb, p);
-			if (predicate != null) {
-				predicates.add(predicate);
-			}
-		}
-		return !predicates.isEmpty() ? predicates.toArray(new Predicate[] {}) : null;
-	}
-
-	public void setFilters(Map<String, String> filters) {
-		this.context.setFilters(filters);
 	}
 
 	public List<T> findAll() {
@@ -60,10 +43,8 @@ public class JPAQueryFilter<T> implements Serializable {
 		CriteriaQuery<T> cq = cb.createQuery(getBeanClass());
 		Root<T> p = cq.from(getBeanClass());
 
-		Predicate[] predicates = getPredicates(cb, p);
-		if (predicates != null && predicates.length > 0) {
-			cq.where(predicates);
-		}
+		addPredicatesToQuery(cq, cb, p);
+		addOrderToQuery(cq, cb, p);
 
 		TypedQuery<T> query = getEntityManager().createQuery(cq);
 
@@ -83,14 +64,22 @@ public class JPAQueryFilter<T> implements Serializable {
 
 		cq.select(cb.count(p));
 
-		Predicate[] predicates = getPredicates(cb, p);
-		if (predicates != null && predicates.length > 0) {
-			cq.where(predicates);
-		}
+		addPredicatesToQuery(cq, cb, p);
 
 		TypedQuery<Long> query = getEntityManager().createQuery(cq);
 
 		return query.getSingleResult().intValue();
+	}
+
+	private <X> void addPredicatesToQuery(CriteriaQuery<X> cq, CriteriaBuilder cb, Root<T> p) {
+		Predicate[] predicates = helper.getPredicates(cb, p, this.getClass());
+		if (predicates != null && predicates.length > 0) {
+			cq.where(predicates);
+		}
+	}
+
+	private <X> void addOrderToQuery(CriteriaQuery<X> cq, CriteriaBuilder cb, Root<T> p) {
+
 	}
 
 	protected Class<T> getBeanClass() {
--- demoiselle-query-filter/src/main/java/br/jus/tre_pa/frameworkdemoiselle/query/filter/core/operations/SingularOperation.java
+++
@@ -1,9 +0,0 @@
-package br.jus.tre_pa.frameworkdemoiselle.query.filter.core.operations;
-
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
-
-public interface SingularOperation {
-	public <T, X> Predicate execute(CriteriaBuilder cb, Root<T> p, String field, X value);
-}
